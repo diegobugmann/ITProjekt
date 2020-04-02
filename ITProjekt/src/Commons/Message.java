bbf147d0bb1e2443e914338fde439ad85e37b596
MMB360A <michael.berger@eclipso.ch>
2020-04-02 21:00:28 +0200

    Message

--- ITProjekt/src/Commons/Message.java
+++ ITProjekt/src/Commons/Message.java
@@ -1,5 +1,36 @@
 package Commons;
 
-public class Message {
+import java.io.Serializable;
 
+public class Message implements Serializable{
+
+	// Data included in a message
+	private String client;
+	private long id;
+    private long timestamp;
+    
+    // Generator for a unique message ID
+    private static long messageID = 0;
+
+    /**
+     * Increment the global messageID
+     * Code from Bradley Richards
+     * @return the next valid ID
+     */
+    private static long nextMessageID() {
+        return messageID++;
+    }
+    
+    public Message() {
+    	this.id = nextMessageID();
+    }
+    
+    
+    
+    public String toString() {
+    	return "";
+    }
+    
+    
+    
 }
