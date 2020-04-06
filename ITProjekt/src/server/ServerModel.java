ef95eea204d92ce028bb117c55b4bc48bbe7cbec
Diego Bugmann <digi.bugi@hotmail.com>
2020-04-06 19:24:56 +0200

    Start Kommunikation Serverseitig

--- ITProjekt/src/server/ServerModel.java
+++ ITProjekt/src/server/ServerModel.java
@@ -1,15 +1,51 @@
 package server;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class ServerModel {
 	
-	private ArrayList<User> clients;
-	private ArrayList<Game> games;
+	private ObservableList<User> clients = FXCollections.observableArrayList();
+	private ObservableList<Game> games = FXCollections.observableArrayList();
+	private ServerSocket listener;
+	
+	private int port = 8080;
 	
 	public ServerModel() {
-		clients = new ArrayList<User>();
-		games = new ArrayList<Game>();
+		//diese Infos bei Server mit GUI in Methode startServer(), die vom Contr aufgerufen wird
+		try { 
+			listener = new ServerSocket(port, 10, null);
+			Runnable run = new Runnable() {
+				@Override
+				public void run() {
+					while (true) { // !stop
+						try {
+							Socket socket = listener.accept(); // wait for clients to connect
+							User user = new User(ServerModel.this, socket);
+							clients.add(user);
+						} catch (Exception e) {
+							//logger.info(e.toString());
+						}
+					}
+				}
+			};
+			Thread clientThread = new Thread(run, "ServerSocket");
+			clientThread.start(); //Starten des threads und somit aufrufen von run()
+		} catch (IOException e) {
+			//logger.info(e.toString());
+		}
+		
+	}
+	
+	public void broadcast(Message msg) {
+		//logger.info("Broadcasting message to clients");
+		for (User u : clients) {
+			u.send(outMsg); //Methode, um Msg zu versenden
+		}
 	}
 	
 }
--- ITProjekt/src/server/User.java
+++ ITProjekt/src/server/User.java
@@ -7,9 +7,37 @@ public class User {
 	private int userID;
 	private String name;
 	private Socket clientSocket;
+	private ServerModel model;
 	
-	public User(Socket clientSocket, String name) {
+	public User(ServerModel model, Socket clientSocket) {
 		this.clientSocket = clientSocket;
+		this.model = model;
+		
+		Runnable run = new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					Message msg = Message.receive(socket); //Methode, die auf Msges wartet
+					
+					/* Hier wird entschieden, bei welchen Msgs was gemacht wird
+					 
+					if (msg instanceof ChatMsg) {				
+						model.broadcast((ChatMsg) msg);
+					} else if (msg instanceof JoinMsg) {
+						Client.this.name = ((JoinMsg) msg).getName();
+					}
+					
+					*/
+					
+				}
+			}
+		};
+		Thread thread = new Thread(run);
+		thread.start();
+		
+	}
+	
+	public void setName(String name) {
 		this.name = name;
 	}
 
