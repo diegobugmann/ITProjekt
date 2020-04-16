5e27435b961b66b48ba692e03e3f98f45ec00af0
MMB360A <michael.berger@eclipso.ch>
2020-04-16 18:08:26 +0200

    ANpassungen

--- ITProjekt/src/Commons/Message_Stich.java
+++ ITProjekt/src/Commons/Message_Stich.java
@@ -1,12 +1,24 @@
 package Commons;
-
+/**
+ * Sent from Server to all players the Stich for display, the logic about stich such as whom is the next player is made on the Server
+ * @author mibe1
+ *
+ */
 public class Message_Stich extends Message{
-	private String player;
 	private int points;
 	private int StichId;
-	
-	public Message_Stich() {
+	public Message_Stich(int points, int stichId) {
 		super();
+		this.points = points;
+		StichId = stichId;
 	}
+	public int getPoints() {
+		return points;
+	}
+	public int getStichId() {
+		return StichId;
+	}
+	
+
 
 }
